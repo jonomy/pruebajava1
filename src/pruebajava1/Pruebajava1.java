22c11fccb9611a3ad1bdcc83aa5eed6bbf4d7430
jonomy <ceciliabelmar12345@gmail.com>
2017-10-30 20:52:35 -0300

    proyecto de operacion de un area

---
+++ src/pruebajava1/Pruebajava1.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/*******************************************************************************
+ *
+ *
+ * @autor LABO3
+ * fecha:octubre 30 de 2017
+ * programa:pruebajava1
+ * Responsabilidad:clase principal
+ *
+ */
+public class Pruebajava1 {
+    
+    /**
+    /**
+     * @param args the command line arguments
+     */
+    public static /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/*******************************************************************************
+ *cecilia
+ * @author LP 13
+ * Fecha:Octubre 30 de 20017
+ * Programa: Clase3A
+ * Responsabilidad: clase principal
+*****************************************************************************/
+//metodo constructor
+
+
+
+
+public static void main (string)[] arg)´{
+    string. out. println("por favor ingrese el area");
+    string .out.println("por favaor ingrese base");
+    string.out.println("por favor ingrese altura")
+    //Aplicacion del proyecto
+    // Todo code application logic hare
+            
+}
+float Area = restangulo
+float base = restangulo
+float altura= restangulo
